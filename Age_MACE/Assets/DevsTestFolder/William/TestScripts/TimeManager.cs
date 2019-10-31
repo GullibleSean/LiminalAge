141d1c490493c2dd780c86669ddcaa7a3ea2fd90
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-10-31 14:25:39 -0700

    Added base faolders for each developers. (Sean)

---
+++ Age_MACE/Assets/DevsTestFolder/William/TestScripts/TimeManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+     public Slider slider;
+
+    [SerializeField]
+    float SummerDuration = 10.0f;
+    [SerializeField]
+    float FallDuration = 10.0f;
+    [SerializeField]
+    float WinterDuration = 10.0f;
+    [SerializeField]
+    float SpringDuration = 10.0f;
+
+    [SerializeField]
+    float SummTransFallDuration = 5.0f;
+    [SerializeField]
+    float FallTransWintDuration = 5.0f;
+    [SerializeField]
+    float WintTransSpriDuration = 5.0f;
+
+    [SerializeField]
+    float TotalDuration;
+
+    [SerializeField]
+    float TimeTracker;
+
+    float SummerStage;
+    float STFStage;
+    float FallStage;
+    float FTWStage;
+    float WinterStage;
+    float WTSStage;
+    float SpringStage;
+
+    public float GetCurrentTime()
+    {
+        return TimeTracker;
+    }
+
+    public float GetSTFDuration()
+    {
+        return SummTransFallDuration;
+    }
+
+    public float GetFTWDuration()
+    {
+        return FallTransWintDuration;
+    }
+    
+    public float GetWTSDuration()
+    {
+        return WintTransSpriDuration;
+    }
+
+
+    public float GetSummerStage()
+    {
+        return SummerStage;
+    }
+
+    public float GetSTFStage()
+    {
+        return STFStage;
+    }
+
+    public float GetFallStage()
+    {
+        return FallStage;
+    }
+    
+    public float GetFTWStage()
+    {
+        return FTWStage;
+    }
+
+    public float GetWinterStage()
+    {
+        return WinterStage;
+    }
+
+    public float GetWTSStage()
+    {
+        return WTSStage;
+    }
+
+    public float GetSpringStage()
+    {
+        return SpringStage;
+    }
+
+
+    void Awake()
+    {
+        TotalDuration = SummerDuration + SummTransFallDuration + FallDuration + FallTransWintDuration + WinterDuration + WintTransSpriDuration + SpringDuration;
+        TimeTracker = 0.0f;
+
+        SummerStage = SummerDuration;
+        STFStage = SummerStage + SummTransFallDuration;
+        FallStage = STFStage + FallDuration;
+        FTWStage = FallStage + FallTransWintDuration;
+        WinterStage = FTWStage + WinterDuration;
+        WTSStage = WinterStage + WintTransSpriDuration;
+        SpringStage = WTSStage + SpringDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TimeTracker < TotalDuration)
+        {
+            TimeTracker += Time.deltaTime;
+        }
+        slider.value = TimeTracker / TotalDuration;
+    }
+}
---
+++ Age_MACE/Assets/DevsTestFolder/William/TestScripts/WindTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindTrigger : MonoBehaviour
+{
+    public GameObject wind;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var o = other.gameObject.GetComponent<LeafFalling>();
+        if (o)
+        {
+            o.WindIsBlowing();
+            o.SetWindForece(wind.GetComponent<Wind>().GetWindForce());
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var o = other.gameObject.GetComponent<LeafFalling>();
+        if (o)
+        {
+            o.WindIsBlowing();
+            o.SetWindForece(wind.GetComponent<Wind>().GetWindForce());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var o = other.gameObject.GetComponent<LeafFalling>();
+        if (o)
+        {
+            o.WindIsNotBlowing();
+            o.SetWindForece(new Vector3(0f, 0f, 0f));
+        }
+    }
+}
